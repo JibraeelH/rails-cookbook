091642bbd75bbb2c25027627d9e4d485ab256d40
Jibraeel Hassen <jibraeel-hassen@hotmail.co.uk>
2024-09-30 11:26:48 +0100

    Models validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
+
+  validates :recipe_id, presence: true, uniqueness: { scope: :category_id }
+  validates :comment, length: { minimum: 6 }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,8 @@ class Recipe < ApplicationRecord
   has_many :bookmarks
   has_many :categories, through: :bookmarks
 
+  validates :description, presence: true
+  validates :rating, numericality: { in: 0..10 }
+  validates :name, presence: true, uniqueness: true
+
 end
