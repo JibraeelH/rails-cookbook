d677f58c39a7820857659dd9dbef3ea389fd0907
Jibraeel Hassen <jibraeel-hassen@hotmail.co.uk>
2024-09-30 16:36:14 +0100

    routing behaviours

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'categories/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +8,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "categories", to: "categories#index"
+  get "categories/new", to: "categories#new"
+  get "categories/:id", to: "categories#show", as: :category
+  post "categories", to: "categories#create"
+
 end
+
+# get 'categories/new', to: "categories#new"
