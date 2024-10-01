3a12b58672a76b8d00a0fb98b7cde7d9d1c1cc1d
Jibraeel Hassen <jibraeel-hassen@hotmail.co.uk>
2024-10-01 17:43:45 +0100

    Step 3 List of categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,6 +25,7 @@ class CategoriesController < ApplicationController
   def destroy
     @category = Category.find(params[:id])
     @category.destroy
+    redirect_to root_path, status: :see_other
   end
 
   private
